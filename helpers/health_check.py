de4671746693d9499cc2afcfcb6f95010577aa02
Sennett Lau <laub1199@gmail.com>
2022-02-26 13:34:14 +0800

    added ping api health check

--- helpers/health_check.py
+++ helpers/health_check.py
@@ -8,8 +8,12 @@ APP_ENV_IS_DEV = True if os.environ.get("APP_ENV") == 'dev' else False
 
 def server_health_check(context: CallbackContext):
     r = requests.get("https://cityuge.com")
-    print('[Health Check]Server status code: {}'.format(str(r.status_code)))
-    if (not r.status_code == 200):
+    print('[Health Check]Server index status code: {}'.format(str(r.status_code)))
+    ping = requests.get("https://cityuge.com/api/ping")
+    print('[Health Check]Server ping status code: {}'.format(str(ping.status_code)))
+    if (not r.status_code == 200 or not ping.status_code == 200):
         for chat_id in GROUP_IDS:
-            text = 'Server is down with status code: {}'.format(str(r.status_code))
+            text = '🚨🚨🚨 Server is down 🚨🚨🚨'
+            text += '\nIndex page status code: {}'.format(str(r.status_code))
+            text += '\nPing api status code: {}'.format(str(ping.status_code))
             context.bot.sendMessage(chat_id=chat_id, text=text, parse_mode='HTML')
